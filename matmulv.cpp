3bd4f9400a98b959a6d9dedd0957cd5de41226e5
sithuthantsin86 <sithuthantsin86@gmail.com>
2017-03-12 17:32:47 +0300

    matrix vector

--- matmulv.cpp
+++ matmulv.cpp
@@ -3,16 +3,24 @@
 #include <stdlib.h>
 #include <chrono>
 using namespace std;
-void serial(int *a, int *b, int *c, int R, int C){
+void Serial(int *a, int *b, int *c, int R, int C){
 	for(int i = 0; i < R; ++i)
         for(int j = 0; j < C; ++j)
             for(int k = 0; k < C; ++k)
             {
                 c[i*C+j] += a[i*C+k] * b[k*C+j];
             }
-
 }
-void parallel_without_vector(int *a, int *b, int *c, int R, int C){
+void Only_vector(int *a, int *b, int *c, int R, int C){
+	for(int i = 0; i < R; ++i)
+        for(int j = 0; j < C; ++j)
+        	#pragma omp simd //reduction (+:c[i*C+j])
+            for(int k = 0; k < C; ++k)
+            {
+                c[i*C+j] += a[i*C+k] * b[k*C+j];
+            }
+}
+void Parallel_without_vector(int *a, int *b, int *c, int R, int C){
 	#pragma omp parallel for
 	for(int i = 0; i < R; ++i)
         for(int j = 0; j < C; ++j)
@@ -21,17 +29,18 @@ void parallel_without_vector(int *a, int *b, int *c, int R, int C){
                 c[i*C+j] += a[i*C+k] * b[k*C+j];
             }
 }
-void parallel_with_vector(int *a, int *b, int *c, int R, int C){
-	#pragma omp for simd
-	for(int i = 0; i < R; ++i)
-        for(int j = 0; j < C; ++j)
-            for(int k = 0; k < C; ++k)
-            {
-                c[i*C+j] += a[i*C+k] * b[k*C+j];
-            }
+void Parallel_with_vector(int *a, int *b, int *c, int R, int C){
+		#pragma omp parallel for
+		for(int i = 0; i < R; ++i)
+        	for(int j = 0; j < C; ++j)
+        		#pragma omp simd //reduction (+:c[i*C+j])
+            	for(int k = 0; k < C; ++k)
+            	{
+                	c[i*C+j] += a[i*C+k] * b[k*C+j];
+            	}
 }
 int main(int argc, char* argv[]){
-	int *a, *b, *c, R, C, nt;
+	int *a, *b, *c, *d, R, C, nt;
 	if (argc >= 4){
         R = atoi(argv[1]);
         if (argc == 4){
@@ -58,6 +67,11 @@ int main(int argc, char* argv[]){
         cerr << "Error : Your size is too much.\n";
         exit(1);
     }
+    d = (int *) malloc(R * C * sizeof (int));
+	if (d == NULL) {
+        cerr << "Error : Your size is too much.\n";
+        exit(1);
+    }
     for(int i = 0; i < R; ++i)
         for(int j = 0; j < C; ++j)
         {
@@ -65,17 +79,26 @@ int main(int argc, char* argv[]){
             b[i*C+j]=i*i;
             c[i*C+j]=0;
         }
+        
     auto start0 = std::chrono::high_resolution_clock::now();
-    serial(a, b, c, R, C);
+    Serial(a, b, c, R, C);
     auto end0 = std::chrono::high_resolution_clock::now();
     cout << "Serial run time in millisecs: " << (std::chrono::duration_cast<chrono::milliseconds>(end0 - start0)).count() << "\n";
+
     auto start1 = std::chrono::high_resolution_clock::now();
-    parallel_without_vector(a, b, c, R, C);
+    Only_vector(a, b, c, R, C);
     auto end1 = std::chrono::high_resolution_clock::now();
-    cout << "Parallel without vector run time in millisecs: " << (std::chrono::duration_cast<chrono::milliseconds>(end1 - start1)).count() << "\n";
+    cout << "Only vector run time in millisecs: " << (std::chrono::duration_cast<chrono::milliseconds>(end1 - start1)).count() << "\n";
+
     auto start2 = std::chrono::high_resolution_clock::now();
-    parallel_with_vector(a, b, c, R, C);
+    Parallel_without_vector(a, b, c, R, C);
     auto end2 = std::chrono::high_resolution_clock::now();
-    cout << "Parallel with vector run time in millisecs: " << (std::chrono::duration_cast<chrono::milliseconds>(end2 - start2)).count() << "\n";
+    cout << "Parallel without vector run time in millisecs: " << (std::chrono::duration_cast<chrono::milliseconds>(end2 - start2)).count() << "\n";
+
+    auto start3 = std::chrono::high_resolution_clock::now();
+    Parallel_with_vector(a, b, c, R, C);
+    auto end3 = std::chrono::high_resolution_clock::now();
+    cout << "Parallel with vector run time in millisecs: " << (std::chrono::duration_cast<chrono::milliseconds>(end3 - start3)).count() << "\n";
+
 	return 0;
 }
